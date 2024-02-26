34bbf098b708419689fe4842e712bac104db4ca2
Pricila Oliveira <PRICILAOLIVEIRAS21@OUTLOOK.COM.BR>
2024-02-26 17:00:27 -0300

    GuessNumber

--- GuessNumber/main.py
+++ GuessNumber/main.py
@@ -1,1 +1,35 @@
+# Importando Lib
 import random
+
+print("Seja bem vindo ao Guess Number.")
+choiceNumber = input("Digite um número teto para o desafio: ")
+
+if choiceNumber.isdigit():
+   choiceNumber = int(choiceNumber)
+else:
+   print("Erro: Valor informado não é numérico. Tente novamente.")
+   quit()
+
+randomNumber = random.randint(0, choiceNumber)
+
+countingChoices = 0
+
+while True:
+    answerUser = input("Advinhe o número: ")
+
+    countingChoices = countingChoices +1
+    if answerUser.isdigit():
+        answerUser = int(answerUser)
+    else:
+        print("Erro: Valor informado não é numérico. Tente novamente.")    
+        continue
+    
+    if answerUser == randomNumber:
+        print("Você acertou!!!")
+        break
+    elif answerUser > randomNumber:
+        print("Chutou alto, o número é menor.")
+    else:
+        print("Chutou baixo, o número é maior.")
+
+print("Número de tentativas: " + str(countingChoices))
